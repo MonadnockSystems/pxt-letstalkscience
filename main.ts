c2eed34927720fe9989ee68464a3fc204d73ddd4
Shawn Nock <shawn@monadnock.ca>
2018-03-21 15:14:12 -0400

    Camelcase for readablity and light sensor

--- main.ts
+++ main.ts
@@ -11,7 +11,15 @@ namespace lts {
         MEASURE_HUMID = 0xE5,
     };
 
+    enum TSL2561 {
+        ADDR = 0x39,
+        SERIAL = 0x8A,
+        VISIBLE = 0x8C,
+    }
+
     let co2_raw = 0;
+    let lightSensorPresent = false;
+    let haveCheckedLightSensor = false;
 
     function reset_temp_humid() {
         pins.i2cWriteNumber(
@@ -22,8 +30,53 @@ namespace lts {
         );
     }
 
+    function checkLightSensor() : boolean {
+        pins.i2cWriteNumber(TSL2561.ADDR, TSL2561.SERIAL, NumberFormat.UInt8LE);
+        let serial_num = pins.i2cReadNumber(TH.ADDR, NumberFormat.UInt8LE);
+        if (serial_num == 0x0) {
+            serial.writeLine("Couldn't find light sensor, disabling.");
+            return false;
+        } else {
+            let part = serial_num >> 4;
+            switch (part) {
+                case 0x0:
+                    serial.writeString("Light Sensor is TSL2560");
+                    break;
+                case 0x1:
+                    serial.writeString("Light Sensor is TSL2561");
+                    break;
+                default:
+                    serial.writeString("Bad light sensor part code: " + part);
+                    return false;
+            }
+            let rev = serial_num & 0xf;
+            serial.writeLine(", rev. " + rev);
+            return true;
+        }
+    }
+
     //% block
-    export function start_science() {
+    export function haveLightSensor() : boolean {
+        return lightSensorPresent
+    }
+
+    function get_light_raw() {
+        pins.i2cWriteNumber(TSL2561.ADDR, TSL2561.VISIBLE, NumberFormat.UInt8LE);
+        let raw = pins.i2cReadNumber(TH.ADDR, NumberFormat.UInt16LE);
+        return raw;
+    }
+
+    //% block
+    export function lux() {
+        // Eventually do some real maths
+        if (haveLightSensor() != true) {
+            return "";
+        }
+        return "" + get_light_raw();
+    }
+
+    //% block
+    export function startSensorBoard() {
         serial.redirect(
             SerialPin.P15,
             SerialPin.P16,
@@ -35,6 +88,8 @@ namespace lts {
         });
 
         reset_temp_humid();
+
+        lightSensorPresent = checkLightSensor();
     }
 
     function get_raw_temp() {
@@ -54,33 +109,33 @@ namespace lts {
     }
 
     function decimalize(n : number) {
-        return n/100 + "." + n%100;
+        return n/100 + "." + n%100 / 10;
     }
 
     //% block
-    export function get_co2_ppm() {
+    export function co2() {
         return 5 * (co2_raw / 1000 - 2);
     }
 
-    //% block="Temperature" icon="\uf135"
-    export function get_temp_number() {
+    //% block
+    export function temperature() {
         return get_raw_temp() * 17572 / 65536 - 4685;
     }
 
     //% block
-    export function get_temp_string() {
-        let r = get_temp_number();
+    export function temperatureString() {
+        let r = temperature();
         return decimalize(r);
     }
 
     //% block
-    export function get_rh_number() {
+    export function humidity() {
         return get_raw_humid() * 12500 / 65536 - 600;
     }
 
     //% block
-    export function get_rh_string() {
-        let r = get_rh_number();
+    export function humidityString() {
+        let r = humidity();
         return decimalize(r);
     }
 
--- test.ts
+++ test.ts
@@ -1,12 +1,26 @@
 // tests go here; this will not be compiled when this package is used as a library
-
-music.beginMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once);
-lts.start_science();
-basic.pause(1000);
-serial.writeLine("CO2 (ppm),temperature (deg. C),relative humidity (%)")
+music.beginMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once)
+basic.showLeds(`
+    . . . . .
+    . # . # .
+    . . . . .
+    # . . . #
+    . # # # .
+    `)
+basic.pause(1000)
+for (let index = 0; index <= 9; index++) {
+    basic.showNumber(9 - index)
+    basic.pause(1000)
+}
+lts.startSensorBoard()
 basic.forever(() => {
-    serial.writeString(lts.get_co2_ppm() + ",");
-    serial.writeString(lts.get_temp_string() + ",");
-    serial.writeLine(lts.get_rh_string());
-    basic.pause(5000);
+    serial.writeString("" + lts.co2() + ",")
+    serial.writeString("" + lts.temperatureString() + ",")
+    if (lts.haveLightSensor()) {
+        serial.writeString(lts.lux() + ",")
+    }
+    serial.writeLine(lts.humidityString())
+    basic.showString("" + lts.co2() + " ppm")
+    basic.showString("" + lts.temperatureString() + " C")
+    basic.showString("" + lts.humidityString() + " %RH")
 })
