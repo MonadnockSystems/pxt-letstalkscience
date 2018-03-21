32b2fed5fbfead8a20a32346e3b48109fe56f75a
Shawn Nock <shawn@monadnock.ca>
2018-03-21 11:44:35 -0400

    Adds test, removes radio dep, and refactors into namespace

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 /**
  * Provides access to LTS Sensor Board functionality.
  */
-//% color=190 weight=100 icon="\uf1ec" block="Let's Talk Science!"
+//% color=190 weight=100 icon="\uf135" block="Let's Talk Science!"
 namespace lts {
 
     enum TH {
@@ -9,52 +9,79 @@ namespace lts {
         RESET = 0xFE,
         MEASURE_TEMP = 0xE3,
         MEASURE_HUMID = 0xE5,
+    };
+
+    let co2_raw = 0;
+
+    function reset_temp_humid() {
+        pins.i2cWriteNumber(
+            TH.ADDR,
+            TH.RESET,
+            NumberFormat.UInt8LE,
+            false
+        );
     }
 
-    class lts_board {
-        co2_pin : DigitalPin;
-        co2_ppm : number;
+    //% block
+    export function start_science() {
+        serial.redirect(
+            SerialPin.P15,
+            SerialPin.P16,
+            BaudRate.BaudRate115200
+        );
 
-        constructor() {
-            this.co2_pin = DigitalPin.P13;
-            this.co2_ppm = 0;
+        pins.onPulsed(DigitalPin.P13, PulseValue.High, () => {
+            co2_raw = pins.pulseDuration();
+        });
 
-            serial.redirect(
-                SerialPin.P15,
-                SerialPin.P16,
-                BaudRate.BaudRate115200
-            );
-
-            pins.onPulsed(this.co2_pin, PulseValue.High, () => {
-                this.co2_ppm = (pins.pulseDuration() / 1000 - 2) * 5;
-            });
-
-            this.reset_temp_humid();
-        }
-
-        reset_temp_humid() {
-            pins.i2cWriteNumber(
-                TH.ADDR,
-                TH.RESET,
-                NumberFormat.UInt8LE,
-                false
-            )
-        }
-
-        get_raw_temp() {
-            pins.i2cWriteNumber(TH.ADDR, TH.MEASURE_TEMP, NumberFormat.UInt8LE);
-            let raw = pins.i2cReadNumber(TH.ADDR, NumberFormat.UInt16BE);
-            return raw;
-        }
-
-        get_raw_humid() {
-            pins.i2cWriteNumber(TH.ADDR, TH.MEASURE_HUMID, NumberFormat.UInt8LE);
-            let raw = pins.i2cReadNumber(TH.ADDR, NumberFormat.UInt16BE);
-            return raw;
-        }
-
-        get_raw_co2() {
-            return this.co2_ppm;
-        }
+        reset_temp_humid();
     }
+
+    function get_raw_temp() {
+        pins.i2cWriteNumber(TH.ADDR, TH.MEASURE_TEMP, NumberFormat.UInt8LE);
+        let raw = pins.i2cReadNumber(TH.ADDR, NumberFormat.UInt16BE);
+        return raw;
+    }
+
+    function get_raw_humid() {
+        pins.i2cWriteNumber(TH.ADDR, TH.MEASURE_HUMID, NumberFormat.UInt8LE);
+        let raw = pins.i2cReadNumber(TH.ADDR, NumberFormat.UInt16BE);
+        return raw;
+    }
+
+    function get_raw_co2() {
+        return co2_raw;
+    }
+
+    function decimalize(n : number) {
+        return n/100 + "." + n%100;
+    }
+
+    //% block
+    export function get_co2_ppm() {
+        return 5 * (co2_raw / 1000 - 2);
+    }
+
+    //% block="Temperature" icon="\uf135"
+    export function get_temp_number() {
+        return get_raw_temp() * 17572 / 65536 - 4685;
+    }
+
+    //% block
+    export function get_temp_string() {
+        let r = get_temp_number();
+        return decimalize(r);
+    }
+
+    //% block
+    export function get_rh_number() {
+        return get_raw_humid() * 12500 / 65536 - 600;
+    }
+
+    //% block
+    export function get_rh_string() {
+        let r = get_rh_number();
+        return decimalize(r);
+    }
+
 }
--- test.ts
+++ test.ts
@@ -1,1 +1,12 @@
 // tests go here; this will not be compiled when this package is used as a library
+
+music.beginMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once);
+lts.start_science();
+basic.pause(1000);
+serial.writeLine("CO2 (ppm),temperature (deg. C),relative humidity (%)")
+basic.forever(() => {
+    serial.writeString(lts.get_co2_ppm() + ",");
+    serial.writeString(lts.get_temp_string() + ",");
+    serial.writeLine(lts.get_rh_string());
+    basic.pause(5000);
+})
